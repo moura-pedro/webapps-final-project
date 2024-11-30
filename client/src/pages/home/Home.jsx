88ef634c1acffa0e23b0027105987efb00f64128
Xander <xanderlockard14@gmail.com>
2024-11-30 15:36:42 -0500

    Added movie cards, movie list, and logic to get movies from backend

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css';
-import Navbar from '../../components/navbar/Navbar';
+import MovieList from '../../components/movielist/MovieList';
 
 const Home = () => {
+  const [moviesByGenre, setMoviesByGenre] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getMovies = async () => {
+      try {
+        const response = await fetch('/api/movies/get-movies', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+
+        const json = await response.json();
+
+        if (json.success) {
+          setMoviesByGenre(json.movies);
+        }
+      } catch (err) {
+        console.log('Something went wrong', err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getMovies();
+  }, []);
+
+  if (loading) {
+    return <div></div>;
+  }
+
   return (
     <div className="home-container">
+      <ul>
+        {Object.entries(moviesByGenre).map(([genre, movies]) => (
+          <li key={genre}>
+            <MovieList genre={genre} movies={movies} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
